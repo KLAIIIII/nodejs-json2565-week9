6adca72b90d4c5eb4493c23c2def491088183437
KLAIIIII <guyyg03546@gmail.com>
2022-07-11 17:33:36 +0700

    Update

--- index.js
+++ index.js
@@ -1,15 +1,18 @@
 const http = require('http')
 const msg = require('./message.js')
-const {loadUser}  = require('./user_models')
+// const {loadUser}  = require('./user_models')
+const {loadVnl2022} = require('./vnl2022.json')
 
 const host = "localhost"
 const PORT = process.env.PORT || 5000
 
 const server = http.createServer((req, res) => {
-    const JsonContent = JSON.stringify(loadUser())
+    // const JsonContent = JSON.stringify(loadUser())
+    const JsonContent = JSON.stringify(loadVnl2022())
 
     res.writeHead(200, { 'Content-Type':'text/plain' })
     res.write(`${JsonContent}`)
+    // res.write(`${loadVnl2022}`)
     res.end()
 })
 
